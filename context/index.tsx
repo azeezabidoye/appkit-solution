90f4b9f6dec7ac8470f9b0f3fc525ff7762b6feb
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-09 05:12:13 +0100

    Context updated

--- context/index.tsx
+++ context/index.tsx
@@ -7,3 +7,5 @@ import { mainnet, arbitrum } from "@reown/appkit/networks";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+
+const queryClient = new QueryClient();
