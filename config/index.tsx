e54dc2aa9f7281fba7b1a340db01b38d2cedd8d9
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-09 04:44:57 +0100

    Config updated

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,5 @@
 import { cookieStorage, createStorage } from "wagmi";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
