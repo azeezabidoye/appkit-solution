d7be1ebd74fdc3a235f58e3e2b22fe7f6a245dcd
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-09 07:37:42 +0100

    Layout config updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,3 +3,26 @@ import { Inter } from "next/font/google";
 import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+import { headers } from "next/headers";
+import ContextProvider from "@/context";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Appkit solution",
+  description: "Powered by Reown",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const cookies = headers().get("cookie");
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+      </body>
+    </html>
+  );
+}
