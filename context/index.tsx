f2403e7f90f14787f5fca8357693d573bd01d36d
azeezabidoye <azeezabidoye@yahoo.com>
2024-11-09 05:03:00 +0100

    Context updated

--- context/index.tsx
+++ context/index.tsx
@@ -3,3 +3,5 @@
 import { wagmiAdapter, projectId } from "@/config";
 import { createAppKit } from "@reown/appkit";
 import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+import { QueryClien, QueryClientProvidert } from "@tanstack/react-query";
